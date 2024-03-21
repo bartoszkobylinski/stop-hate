a6f543968ba5c080b3c440c2fcc5cef681c59e28
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2024-03-21 14:12:10 +0100

    Refactor auth to TwitterClient class with user info method

--- twitter_connection.py
+++ twitter_connection.py
@@ -2,18 +2,33 @@ import os
 import tweepy
 
 
-api_key = os.getenv("API_KEy")
-api_secret = os.getenv("API_SECRET_KEY")
-access_token = os.getenv("ACCeSS_TOKEN")
-access_token_secret = os.getenv("ACCES_TOKEN_SECRET")
+class TweeterCLient:
 
-auth = tweepy.OAuthHandler(api_key, api_secret)
-auth.set_access_token(access_token, access_token_secret)
+    def __init__(self):
+        api_key = os.getenv("API_KEy")
+        api_secret = os.getenv("API_SECRET_KEY")
+        access_token = os.getenv("ACCeSS_TOKEN")
+        access_token_secret = os.getenv("ACCES_TOKEN_SECRET")
 
-api = tweepy.API(auth)
+        auth = tweepy.OAuthHandler(api_key, api_secret)
+        auth.set_access_token(access_token, access_token_secret)
 
-try:
-    api.verif_cedentials()
-    print("auth ok")
-except:
-    print("something went wongi")
+        self.api = tweepy.API(auth)
+
+        try:
+            self.api.verify_cedentials()
+            print("auth ok")
+        except:
+            print("something went wongi")
+
+    def get_user_info(self, user_id):
+        try:
+            user = self.api.get_user(user_id=user_id)
+            return {
+                "name": user.name,
+                "description": user.description,
+                "followers_count": user.followers_count
+            }
+        except Exception as error:
+            return {"error": str(error)}
+
