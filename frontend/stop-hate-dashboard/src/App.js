4d3b65750983dad9f96c47f3870aaa0eb2d7637b
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2024-03-28 20:19:04 +0100

    Adding a basic dashboard component

--- frontend/stop-hate-dashboard/src/App.js
+++ frontend/stop-hate-dashboard/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorldButton from "./HelloWorldButton";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
         <HelloWorldButton />
+        <Dashboard />
 
       </header>
     </div>
---
+++ frontend/stop-hate-dashboard/src/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Dashboard(){
+    return (
+    <div className="dashboard">
+        <div className="square">Square 1</div>
+        <div className="square">Square 2</div>
+        <div className="square">Square 3</div>
+        <div className="square">Square 4</div>
+    </div>
+
+    );
+}
+
+export default Dashboard;
