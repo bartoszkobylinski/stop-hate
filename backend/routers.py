d92c811b2f463e3d871d4d2f1a69bd0e8ee39920
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2024-03-31 19:58:13 +0200

    Adding a logic to displaying user-media

--- backend/routers.py
+++ backend/routers.py
@@ -1,6 +1,9 @@
+import os
+
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, login_required, logout_user
 from user import User
+from instagram_basic_display_api import InstagramBasicDisplayAPI
 
 def init_app_routes(app):
     @app.route("/")
@@ -41,7 +44,24 @@ def init_app_routes(app):
     @app.route("/user-media")
     @login_required
     def user_media():
-        return render_template("user-media.html")
+        code = request.args.get("code")
+        if not code:
+            ig_api = InstagramBasicDisplayAPI({
+                "INSTAGRAM_APP_ID": os.getenv("INSTAGRAM_APP_ID"),
+                "INSTAGRAM_APP_SECRET": os.getenv("INSTAGRAM_APP_SECRET"),
+                "INSTAGRAM_APP_REDIRECT_URI": os.getenv("INSTAGRAM_APP_REDIRECT_URI")
+            })
+            ig_api.set_authorization_url()
+            return redirect(ig_api.authorization_url)
+        else:
+            ig_api = InstagramBasicDisplayAPI({
+                "INSTAGRAM_APP_ID": os.getenv("INSTAGRAM_APP_ID"),
+                "INSTAGRAM_APP_SECRET": os.getenv("INSTAGRAM_APP_SECRET"),
+                "INSTAGRAM_APP_REDIRECT_URI": os.getenv("INSTAGRAM_APP_REDIRECT_URI")
+            })
+            ig_api.set_user_instagram_access_token(code)
+            user_media = ig_api.get_user_media()
+            return render_template("user-media.html", media_data=user_media)
 
     @app.route("/my-page")
     @login_required
