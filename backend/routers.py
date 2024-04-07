c00a61b4ba553c797d164ce3cf544fc680d8fb19
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2024-04-07 08:29:37 +0200

    Adding some error handling and removing unnecessary function to get current user

--- backend/routers.py
+++ backend/routers.py
@@ -5,10 +5,7 @@ from flask_login import login_user, login_required, logout_user, current_user
 from user import User
 from instagram_basic_display_api import InstagramBasicDisplayAPI
 
-def get_current_user():
-    if current_user.is_authenticated:
-        return current_user
-    return None
+
 
 def init_app_routes(app):
     @app.route("/")
@@ -24,6 +21,9 @@ def init_app_routes(app):
         username = request.form['username']
         password = request.form['password']
 
+        if not username or not password:
+            flash("Username and password are required")
+            return redirect(url_for("login"))
 
         user = User.authenticate(username, password)
 
@@ -50,23 +50,23 @@ def init_app_routes(app):
     @login_required
     def user_media():
         code = request.args.get("code")
-        if not code:
+        try:
             ig_api = InstagramBasicDisplayAPI({
                 "INSTAGRAM_APP_ID": os.getenv("INSTAGRAM_APP_ID"),
                 "INSTAGRAM_APP_SECRET": os.getenv("INSTAGRAM_APP_SECRET"),
                 "INSTAGRAM_APP_REDIRECT_URI": os.getenv("INSTAGRAM_APP_REDIRECT_URI")
             })
-            ig_api.set_authorization_url()
-            return redirect(ig_api.authorization_url)
-        else:
-            ig_api = InstagramBasicDisplayAPI({
-                "INSTAGRAM_APP_ID": os.getenv("INSTAGRAM_APP_ID"),
-                "INSTAGRAM_APP_SECRET": os.getenv("INSTAGRAM_APP_SECRET"),
-                "INSTAGRAM_APP_REDIRECT_URI": os.getenv("INSTAGRAM_APP_REDIRECT_URI")
-            })
-            ig_api.set_user_instagram_access_token(code)
-            user_media = ig_api.get_user_media()
-            return render_template("user-media.html", media_data=user_media)
+            if not code:
+                ig_api.set_authorization_url()
+                return redirect(ig_api.authorization_url)
+            else:
+                ig_api.set_user_instagram_access_token(code)
+                user_media = ig_api.get_user_media()
+                return render_template("user-media.html", media_data=user_media)
+        except Exception as error:
+            flash("An error occurred while trying to fetch Instagram data.")
+            app.logger.error(f"Instagram Api error: {error}")
+            return redirect(url_for("main"))
 
     @app.route("/my-page")
     @login_required
@@ -81,8 +81,7 @@ def init_app_routes(app):
     @app.route("/admin")
     @login_required
     def admin():
-        current_user = get_current_user()
-        if not current_user.is_admin:
+        if not current_user.is_authenticated or not current_user.is_admin:
             flash("Unauthorized access.")
             return redirect(url_for("main"))
 
