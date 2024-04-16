5f38794dde33ccad1c70201f25a6fcc57e9beff4
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2024-04-16 22:41:37 +0200

    wandering for hours for a typo error. Damn

--- backend/main.py
+++ backend/main.py
@@ -9,8 +9,9 @@ url = f"https://graph.instagram.com/{user_id}/media?fields=id,caption&access_tok
 def get_user_media(access_token, user_id, url):
     url = url
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()['data']
+    if user_id == instagram_user_id:
+        if response.status_code == 200:
+            return response.json()['data']
     else:
         return f"Error: {response.text}"
 
