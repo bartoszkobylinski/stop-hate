596e041499931b1adfc7372adf5ce0a910682048
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2024-04-17 21:28:36 +0200

    moing prompt settings in to seperate file

--- backend/open_ai_conection.py
+++ backend/open_ai_conection.py
@@ -1,18 +1,22 @@
+from prompt_settings import settings
 import openai
 import os
 
 system_api_key = os.getenv("SYSTEM_API_KEY")
 user_apki_key = os.getenv("USER_API_KEY")
 
-def request_open_ai(prompt, api_key, sys_prompt):
+
+def request_open_ai(prompt, api_key, sys_prompt, settings):
     openai.api_key = api_key
     completion = openai.ChatCompletion.create(
-        model='gpt-3.5-tubro',
+        model=settings.model,
         messages=[
             {"role": "system", "content": sys_prompt},
             {"role": "user", "content": prompt}
         ],
-        max_tokens=1280,
-        n=0.7,
-        temperature=0.3
+        max_tokens=settings.max_token,
+        n=settings.n,
+        temperature=settings.temperature
     )
+
+
