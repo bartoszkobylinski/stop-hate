ace1457f1f586a97c8a0091824b3cb03c12e9eb0
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2024-04-20 12:00:55 +0200

    moing prompt settings in to seperate file

--- backend/prompt.py
+++ backend/prompt.py
@@ -9,4 +9,5 @@ valuable_prompt = os.getenv("VALUE_PROMPT")
 user_prompt = os.getenv("USER_PROMPT")
 twitter_prompt = os.getenv("TWITTER_PROMPT")
 instagram_prompt = os.getenv("INSTAGRAM_PROMPT")
+url_for_decisison = os.getenv("URL_PROMPT")
 
