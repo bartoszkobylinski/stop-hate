f46e3e933b759c08788cae23367c55ff0f335cf4
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2024-04-23 23:26:27 +0200

    Refactoring code to class version. Wykop API

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,28 @@
 import requests
 import json
 
-access_token = "abosteuthue"
-user_id = "123"
-url = f"https://graph.instagram.com/{user_id}/media?fields=id,caption&access_token={access_token}"
 
+class InstagramMediaFetcher:
+    def __init__(self, access_token, user_id):
+        self.access_token = access_token
+        self.user_id = user_id
+        self.base_url = f"https://graph.instagram.com/{user_id}/media"
 
-def get_user_media(access_token, user_id, url):
-    url = url
-    response = requests.get(url)
-    if user_id == instagram_user_id:
+    def get_user_media(self):
+        url = f"{self.base_url}?fields=id,caption&access_token={self.access_token}"
+        response = requests.get(url)
         if response.status_code == 200:
             return response.json()['data']
-    else:
-        return f"Error: {response.text}"
+        else:
+            return f"Error: {response.text}"
+
+    def print_media(self):
+        media = self.get_user_media()
+        print(json.dumps(media, indent=1))
 
 
-
-
-
-media = get_user_media(access_token, user_id, url)
-
-print(json.dumps(media, indent=1))
+if __name__ == "__main__":
+    access_token = "abosteuthue"
+    user_id = "123"
+    media_fetcher = InstagramMediaFetcher(access_token, user_id)
+    media_fetcher.print_media()
